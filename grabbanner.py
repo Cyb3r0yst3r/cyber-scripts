94977fa2eb12fb0feb5ea97b3c8d4e6cef2d940d
Bayou Cyber Data Science <80774444+Cyb3r0yst3r@users.noreply.github.com>
2021-04-15 17:46:48 -0500

    Create grabbanner.py

---
+++ grabbanner.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+import socket
+ip = str(input('Enter with IP: '))
+port = int(input('Enter with Port: '))
+
+mysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysocket.connect((ip, port))
+banner = mysocket.recv(1024)
+print(banner)
